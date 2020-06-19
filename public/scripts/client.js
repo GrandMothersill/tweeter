c439a9a6be05a69c99ef81fce3f3802dc6f2d76c
Graham Mothersill <graham.mothersill@gmail.com>
2020-06-19 01:05:21 +0000

    clean code, finesse error messages, build readme and screenshots

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,52 +1,10 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const escape = function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
-}
+};
 
+// creates tweet-container html using input data
 const createTweetElement = function(data) {
   return `
   <article>
@@ -58,27 +16,21 @@ const createTweetElement = function(data) {
   <p>${escape(data.content.text)}</p>
   <hr>
   <footer>
-
-
-
     <span>${moment(data.created_at).fromNow()}</span>
-
-
-
     <i class="fa fa-flag" aria-hidden="true"></i>
     <i class="fa fa-retweet" aria-hidden="true"></i>
     <i class="fa fa-heart" aria-hidden="true"></i>
   </footer>
 </article>
-    `
+    `;
 };
 
+// prepends all tweets of given array
 const renderTweets = (tweetArr) => {
   for (let tweet of tweetArr) {
     $('#tweets-container').prepend(createTweetElement(tweet));
   }
 };
-
 
 $(document).ready(function() {
 
@@ -87,53 +39,95 @@ $(document).ready(function() {
       .then(function(data) {
         $('#tweets-container').empty();
         renderTweets(data);
-      })
+      });
   };
 
+
+  // hide/show new-tweet section
   let shown = false;
 
   $("#nav-new-tweet").on('click', function() {
     if (!shown) {
-      shown = true;
+      $("#nav-new-tweet").html("<strong>Hide</strong> tweet composer");
       $(".new-tweet").slideDown();
       $(".new-tweet").css("display", "block");
-      $("#tweet-text").focus()
+      $("#tweet-text").focus();
+      shown = true;
     } else {
+      $("#nav-new-tweet").html("<strong>Write</strong> a new tweet");
       $(".new-tweet").slideUp();
       shown = false;
     }
-  }),
+  });
 
-    $('#the-form').submit(function(evt) {
-      evt.preventDefault();
-      console.log("Submit registered");
-      $(".isa_error").css("display", "none");
-      let tweetText = $(this).serialize().slice(5)
-      if (tweetText.length === 0) {
-        $(".isa_error").slideDown();
-        $(".isa_error").text("Tweet must contain at least 1 character");
+  $("#sticky-button").on('click', function() {
+    shown = true;
+    $("#nav-new-tweet").html("<strong>Hide</strong> tweet composer");
+    $(".new-tweet").slideDown();
+    $(".new-tweet").css("display", "block");
+    $("#tweet-text").focus();
+    $(window).scrollTop(0);
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+      $("#sticky-button").css("display", "block");
+    } else {
+      $("#sticky-button").css("display", "none");
+    }
+  });
+
+  // submitting a new tweet & error handling
+  let error = false;
+  $('#the-form').submit(function(evt) {
+    evt.preventDefault();
+    $(".isa_error").css("display", "none");
+    let tweetText = $("#tweet-text").val();
+    tweetText = tweetText.trim();
+
+    if (tweetText.length === 0) {
+      error = true;
+      $(".isa_error").slideDown();
+      $(".isa_error").text("Tweet must contain at least 1 character");
+      $(".isa_error").css("display", "block");
+    } else if (tweetText.length > 140) {
+      error = true;
+      $(".isa_error").slideDown();
+      $(".isa_error").text("Tweet must contain less than 140 characters");
+      $(".isa_error").css("display", "block");
+    } else {
+      if (error) {
         $(".isa_error").css("display", "block");
-      } else if (tweetText.length > 140) {
-        $(".isa_error").slideDown();
-        $(".isa_error").text("Tweet must contain under 140 characters");
-        $(".isa_error").css("display", "block");
-      } else {
-        $(".isa_error").css("display", "none");
-        $.ajax({
-          url: '/tweets',
-          method: 'POST',
-          data: $(this).serialize(),
-        })
-          .then(function() {
-            console.log("Hurrah");
-            // $('#tweets-container').empty();
-            $('#tweet-text').val('');
-            $('#char-counter').text(140);
-            $.fn.loadTweets();
-          })
+        $(".isa_error").slideUp();
+        error = false;
       }
-    });
 
+      $.ajax({
+        url: '/tweets',
+        method: 'POST',
+        data: $(this).serialize(),
+      })
+        .then(function() {
+          $('#tweet-text').val('');
+          $('#char-counter').text(140);
+          $.fn.loadTweets();
+        });
+    }
+  });
 
+  //// tweet-input character counter
+  $("#tweet-text").on('keyup', function() {
+    let charsLeft = 140 - this.value.length;
+
+    $('#char-counter').text(charsLeft);
+
+    if (charsLeft < 0) {
+      $('#char-counter').css('color', '#FF0000');
+    } else if (charsLeft >= 0) {
+      $('#char-counter').css('color', '#545149');
+    }
+  });
+
+  //initial load from database
   $.fn.loadTweets();
-});
+}); 
